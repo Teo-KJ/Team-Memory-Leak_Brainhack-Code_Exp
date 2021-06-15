71714baa2509331e60c703d9dc4f066fba442844
sweesenkoh <sweesenworkstation@gmail.com>
2021-06-16 00:56:41 +0800

    add http

--- frontend/flutter_application/lib/models/Stall.dart
+++ frontend/flutter_application/lib/models/Stall.dart
@@ -3,10 +3,34 @@ import 'package:flutter_application/models/HawkerCentre.dart';
 class Stall {
   int id;
   String name;
-  HawkerCentre hawkerCentre;
+  String description;
+  String address;
   List<String> imgURLs;
   double latitude;
   double longitude;
 
-  Stall({this.id, this.name, this.hawkerCentre, this.imgURLs});
+  Stall(
+      {this.id,
+      this.name,
+      this.description,
+      this.address,
+      this.imgURLs,
+      this.latitude,
+      this.longitude});
+
+  factory Stall.fromJson(Map<String, dynamic> json) {
+    List<String> imagesList = new List<String>();
+    int length = json['imgURLs'].length;
+    for (int i = 0; i < length; i++) {
+      imagesList.add(json[i]);
+    }
+    return Stall(
+        id: json['id'],
+        name: json['name'],
+        description: json['description'],
+        address: json['address'],
+        latitude: json['latitude'],
+        longitude: json['longitude'],
+        imgURLs: imagesList);
+  }
 }
--- frontend/flutter_application/lib/services/network.dart
+++ frontend/flutter_application/lib/services/network.dart
@@ -1,1 +1,26 @@
-class NetworkManager {}
+import 'dart:convert';
+
+import 'package:flutter_application/models/Stall.dart';
+import 'package:http/http.dart' as http;
+
+class NetworkManager {
+  static String baseURL = "http://18.136.208.222:3001";
+
+  static Future<List<Stall>> getAllStalls() async {
+    var url = NetworkManager.baseURL + '/hawkers';
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      var result = jsonDecode(response.body);
+      var resultList = [];
+      for (var i = 0; i < result.length; i++) {
+        resultList.add(Stall.fromJson(result[i]));
+      }
+      print(resultList);
+      return [];
+    } else {
+      throw Exception('Failed to load album');
+    }
+  }
+}
