c8911ed61742999a3f19c0d8be0d71c2b964c1ff
sweesenkoh <sweesenworkstation@gmail.com>
2021-06-15 20:37:36 +0800

    mapview added display current location

--- frontend/flutter_application/lib/views/home_page/components/map_view.dart
+++ frontend/flutter_application/lib/views/home_page/components/map_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
+import 'package:location/location.dart';
 
 class HomeMapView extends StatefulWidget {
   const HomeMapView({Key key}) : super(key: key);
@@ -10,30 +12,97 @@ class HomeMapView extends StatefulWidget {
 }
 
 class _HomeMapViewState extends State<HomeMapView> {
+  LatLng currentLocation;
+  List<Marker> markers = [];
+  MapController mapController = MapController();
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentLocation();
+    generateStallsMarkers();
+  }
+
   @override
   Widget build(BuildContext context) {
     return FlutterMap(
+      mapController: mapController,
       options: MapOptions(
-        center: LatLng(10, 10),
+        center: LatLng(0, 0),
         zoom: 13.0,
       ),
       layers: [
         TileLayerOptions(
             urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
             subdomains: ['a', 'b', 'c']),
-        MarkerLayerOptions(
-          markers: [
-            Marker(
-              width: 80.0,
-              height: 80.0,
-              point: LatLng(51.5, -0.09),
-              builder: (ctx) => Container(
-                child: FlutterLogo(),
-              ),
-            ),
-          ],
-        ),
+        MarkerLayerOptions(markers: [
+          ...this.markers,
+          Marker(
+            // current location pin
+            width: 80.0,
+            height: 80.0,
+            point: currentLocation,
+            builder: (ctx) => Container(
+                child: Icon(
+              Entypo.location_pin,
+              size: 50,
+              color: Colors.red,
+            )),
+          ),
+        ]),
       ],
     );
+  }
+
+  void generateStallsMarkers() {
+    // this function takes in a list of stalls objects, and for each stall, generate a location pin
+    var markers = [
+      Marker(
+        width: 80.0,
+        height: 80.0,
+        point: LatLng(0, 0),
+        builder: (ctx) => Container(
+            child: Icon(
+          Entypo.location_pin,
+          size: 50,
+          color: Colors.red,
+        )),
+      ),
+    ];
+
+    this.setState(() {
+      this.markers = markers;
+    });
+  }
+
+  void getCurrentLocation() async {
+    Location location = new Location();
+
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+    LocationData _locationData;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    _locationData = await location.getLocation();
+    var currentLocation = LatLng(_locationData.latitude, _locationData.longitude);
+    mapController.move(currentLocation, 14);
+    this.setState(() {
+      this.currentLocation = currentLocation;
+    });
   }
 }
